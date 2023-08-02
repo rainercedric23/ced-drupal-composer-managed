c96793a72c44357d5e56b80ce82c27f1df23a645
Cedric Mariano <cedric.mariano@getpantheon.com>
2023-08-02 19:32:05 +0800

    add fiddle qs script

---
+++ web/private/scripts/fiddle-qs.php
@@ -0,0 +1,9 @@
+<?php
+// Don't ever sanitize the database on the live environment. Doing so would
+// destroy the canonical version of the data.
+if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
+  echo 'Show ENV variables\n';
+  echo implode('|', $_ENV)
+  
+	echo "\nEnd of ENV variables\n";
+}
